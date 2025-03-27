eaff4820daf6ddb277cead481b6d40efa30e33a9
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-03-27 04:01:53 -0700

    Update exp 3.p-1Triangle Pattern Generator Using Loops.py

--- exp 3.p-1Triangle Pattern Generator Using Loops.py
+++ exp 3.p-1Triangle Pattern Generator Using Loops.py
@@ -5,3 +5,8 @@ for i in range(1, n + 1):  # Outer loop controls the number of rows
         print("*", end="")  # print() function prints without a newline
     print()  # After finishing the inner loop, print a newline
 
+
+rows=5
+for i in range(1,rows + 1):
+    print(i* "*")
+
