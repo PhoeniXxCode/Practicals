e0b31701db7faf8d5326805d971e5eeedcb6fbc9
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-03-27 03:33:56 -0700

    Create program 6.Basic Arithmetic Operations in Python.py

---
+++ program 6.Basic Arithmetic Operations in Python.py
@@ -0,0 +1,21 @@
+.Exploring Basic Arithmetic Operations in Python*: 
+AIM: Write a Python program to explore basic arithmetic operations. The program should prompt the user to enter two numbers and then perform addition, subtraction, multiplication, division, 
+and modulus operations on those numbers. The results of each operation should be displayed to the user.
+
+program:
+num1 = int(input('Enter First number: '))
+num2 = int(input('Enter Second number '))
+add = num1 + num2
+dif = num1 - num2
+mul = num1 * num2
+div = num1 / num2
+floor_div = num1 // num2
+power = num1 ** num2
+modulus = num1 % num2
+print('Sum of ',num1 ,'and' ,num2 ,'is :',add)
+print('Difference of ',num1 ,'and' ,num2 ,'is :',dif)
+print('Product of' ,num1 ,'and' ,num2 ,'is :',mul)
+print('Division of ',num1 ,'and' ,num2 ,'is :',div)
+print('Floor Division of ',num1 ,'and' ,num2 ,'is :',floor_div)
+print('Exponent of ',num1 ,'and' ,num2 ,'is :',power)
+print('Modulus of ',num1 ,'and' ,num2 ,'is :',modulus)
