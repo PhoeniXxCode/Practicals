cbe9c5230cf1f48b0b348b0fbc0cb3b04a374606
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-03-27 03:31:20 -0700

    Create program 3.Developing Conversion Utilities.py

---
+++ program 3.Developing Conversion Utilities.py
@@ -0,0 +1,11 @@
+1)	write a program to convert inches to feet
+    program:
+   inches = float(input("Please enter inches:"))
+   feet = inches / 12
+   print(feet, " Feet
+
+2) Python program to convert rupees to dollars
+Program:
+   rupees = float(input("Please enter rupees:"))
+   dollars = rupees /84
+  print(dollars, " Dollars")
