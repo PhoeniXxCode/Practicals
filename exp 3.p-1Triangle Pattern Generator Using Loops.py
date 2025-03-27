070f6826b329695199c5cf251bd04c6f74053121
Shruti Baviskar <shrutibaviskar224@gmail.com>
2025-03-27 04:02:49 -0700

    Update exp 3.p-1Triangle Pattern Generator Using Loops.py

--- exp 3.p-1Triangle Pattern Generator Using Loops.py
+++ exp 3.p-1Triangle Pattern Generator Using Loops.py
@@ -10,3 +10,16 @@ rows=5
 for i in range(1,rows + 1):
     print(i* "*")
 
+
+2 Inverted Triangle Pattern:
+n = 5  # Number of rows for the triangle
+for i in range(n, 0, -1):  # Start from n and decrease to 1
+    for j in range(i):  # Print i stars
+        print("*", end="")
+    print()  # Print new line
+2)
+rows=5
+for i in range(rows,0,-1 ):
+    print(i* "*")
+
+
